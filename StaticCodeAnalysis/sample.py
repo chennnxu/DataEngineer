3c5045f8b27dc22ff637a2aaf9042f0bf3b9e2b0
Chen Xu <849237780@qq.com>
2023-11-13 16:42:30 +0100

    📝 Add file

---
+++ StaticCodeAnalysis/sample.py
@@ -0,0 +1,12 @@
+"""Static code analysis sample"""
+# pip3 install pylint==2.11.1
+# terminal run pylint sample.py
+
+def add(number1, number2):
+    '''Add function get the sum of two numbers'''
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
