4cd7386109a45fe29b2845f8930aae6c1562c818
Chen Xu <849237780@qq.com>
2023-11-13 18:23:55 +0100

    📝 Add files

---
+++ PythonEnhancement/Packaging/mymath/geometry.py
@@ -0,0 +1,12 @@
+import math
+def area_of_rectangle(length, breadth):
+    """
+    returns the area of a rectangle
+    """
+    return length * breadth
+
+def area_of_circle(radius):
+    """
+    returns the area of a circle
+    """
+    return math.pi * (radius ** 2) 
