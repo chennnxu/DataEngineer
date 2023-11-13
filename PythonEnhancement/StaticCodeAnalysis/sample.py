c8ea5cc8c74ae8225f51f0b34f7c62e0f6f6412c
Chen Xu <849237780@qq.com>
2023-11-13 17:18:24 +0100

    🔥 Add and Remove files

--- PythonEnhancement/StaticCodeAnalysis/sample.py
+++ PythonEnhancement/StaticCodeAnalysis/sample.py
@@ -1,10 +1,16 @@
-"""Static code analysis sample"""
+"""
+Static code analysis sample
+"""
 # pip3 install pylint==2.11.1
 # terminal run pylint sample.py
 
+
 def add(number1, number2):
-    '''Add function get the sum of two numbers'''
+    """
+    Add function get the sum of two numbers
+    """
     return number1 + number2
+
 
 NUM1 = 4
 NUM2 = 5
